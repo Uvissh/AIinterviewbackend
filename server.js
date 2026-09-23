6ec69b366636196db396aa4912990c385b570fd8
Uvissh <vishalbhateria@gmail.com>
2026-09-23 20:18:08 +0530

    second commit

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 
 
 app.use(cors({
-    origin:" http://localhost:5173"
+    origin:"https://aiinterviewfrontend-9onz.onrender.com/"
 }))
 app.use(express.json());
 app.use(interviewRouter);
